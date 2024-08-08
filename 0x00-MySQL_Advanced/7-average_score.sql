a3f4ffe6b44157ab84102969f3d92105300d1031
mikelexx <michael.muri2020@gmail.com>
2024-08-08 19:56:55 +0300

    REMOVE GROUP BY WHEN USING AVG

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -12,8 +12,7 @@ PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
 	SELECT AVG(score) INTO avg_score FROM corrections
-	WHERE user_id = user_id
-	GROUP BY user_id;
+	WHERE user_id = user_id;
 	UPDATE users
 	SET average_score = avg_score
 	WHERE id = user_id;

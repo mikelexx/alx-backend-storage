9654503aa03f4cab62f9e5c277bc3e3e964a3746
mikelexx <michael.muri2020@gmail.com>
2024-08-08 16:57:03 +0300

    USE DELIMITER to stop mysql from confusing inner logic with  as end of whole statement

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,7 +3,7 @@
 -- Context: Nothing related to MySQL, but perfect for user email validation - distribute the logic to the database itself!
 
 DROP TRIGGER IF EXISTS reset_valid_email;
-
+DELIMITER $$
 CREATE
 DEFINER = CURRENT_USER
 TRIGGER reset_valid_email
@@ -13,4 +13,5 @@ BEGIN
     IF OLD.email != NEW.email THEN
         SET NEW.valid_email = 0; 
     END IF;
-END;
+END $$
+DELIMITER ;

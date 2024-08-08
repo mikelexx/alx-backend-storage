522d134f502f6643e8e33823a8e263cd734698cf
mikelexx <michael.muri2020@gmail.com>
2024-08-08 16:37:50 +0300

    try if syntax error is dependent on my machine only

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,17 @@
+-- Write a SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+--
+-- Context: Nothing related to MySQL, but perfect for user email validation - distribute the logic to the database itself!
+
+DROP TRIGGER IF EXISTS reset_valid_email;
+
+CREATE
+DEFINER = CURRENT_USER
+TRIGGER reset_valid_email
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+DROP TRIGGER IF EXISTS update_quantity; 

478681e2c46b1d9cad76e71af48a954922283c5f
mikelexx <michael.muri2020@gmail.com>
2024-08-12 11:02:39 +0300

    8. LIST all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that lists all documents in a
+collection:
+
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+"""
+from typing import List, Union
+from pymongo.cursor import Cursor
+from pymongo.collection import Collection
+
+
+def list_all(mongo_collection: Collection) -> Union[Cursor, List]:
+    cursor = mongo_collection.find()
+    print(type(cursor))
+    if cursor.count() == 0:
+        return []
+    else:
+        return cursor

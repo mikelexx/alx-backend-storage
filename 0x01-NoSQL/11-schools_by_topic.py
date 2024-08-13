9c5a464c99395b07232817f516b333de252b13bf
mikelexx <michael.muri2020@gmail.com>
2024-08-13 10:30:09 +0300

    remove annotations due to requirements

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,11 +7,8 @@ Prototype: def schools_by_topic(mongo_collection, topic):
 mongo_collection will be the pymongo collection object
 topic (string) will be topic searched
 """
-from pymongo.cursor import Cursor
-from pymongo.collection import Collection
 
-
-def schools_by_topic(mongo_collection: Collection, topic: str) -> Cursor:
+def schools_by_topic(mongo_collection, topic):
     """
         function that returns the list of school having a specific
         topic

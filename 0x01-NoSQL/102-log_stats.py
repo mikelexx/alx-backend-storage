4ea5a984335c031859995cef989d816b8bf001ff
mikelexx <michael.muri2020@gmail.com>
2024-08-13 09:42:29 +0300

    15. Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Improve 12-log_stats.py by adding the top 10 of the
+most present IPs in the collection nginx of the database
+logs:
+
+The IPs top must be sorted (like the example below)
+"""
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.logs
+collection = db.nginx
+rows = collection.find()
+print("{} logs".format(rows.count()))
+print("Methods:")
+for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+    print("\tmethod {}: {}".format(method,
+                                   collection.find({
+                                       "method": method
+                                   }).count()))
+print("{} status check".format(
+    collection.find({
+        "method": 'GET',
+        'path': '/status'
+    }).count()))
+print("IPs:")
+for row in collection.aggregate([{
+        '$group': {
+            '_id': '$ip',
+            'count': {
+                '$sum': 1
+            }
+        }
+}, {
+        "$project": {
+            "ip": '$_id',
+            'date': 1,
+            'count': 1
+        }
+}, {
+        "$sort": {
+            "count": -1
+        }
+}, {
+        '$limit': 10
+}]):
+    print(row.get('ip'))

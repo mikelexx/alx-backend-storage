4da8c273def6dfaf91998da34420ad7449b1ee3c
mikelexx <michael.muri2020@gmail.com>
2024-08-13 10:03:56 +0300

    prevent impoting code

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -6,45 +6,44 @@ logs:
 
 The IPs top must be sorted (like the example below)
 """
-from pymongo import MongoClient
-
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-rows = collection.find()
-print("{} logs".format(rows.count()))
-print("Methods:")
-for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
-    print("\tmethod {}: {}".format(method,
-                                   collection.find({
-                                       "method": method
-                                   }).count()))
-print("{} status check".format(
-    collection.find({
-        "method": 'GET',
-        'path': '/status'
-    }).count()))
-print("IPs:")
-for row in collection.aggregate([{
-        '$group': {
-            '_id': '$ip',
-            'count': {
-                '$sum': 1
-            }
-        }
-}, {
-        "$project": {
-            "ip": '$_id',
-            'date': 1,
-            'count': 1
-        }
-}, {
-        "$sort": {
-            "count": -1
-        }
-}, {
-        '$limit': 10
-}]):
-    print(row.get('ip'))
 if __name__ == '__main__':
-    exit()
+    from pymongo import MongoClient
+
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    rows = collection.find()
+    print("{} logs".format(rows.count()))
+    print("Methods:")
+    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        print("\tmethod {}: {}".format(method,
+                                       collection.find({
+                                           "method": method
+                                       }).count()))
+    print("{} status check".format(
+        collection.find({
+            "method": 'GET',
+            'path': '/status'
+        }).count()))
+    print("IPs:")
+    for row in collection.aggregate([{
+            '$group': {
+                '_id': '$ip',
+                'count': {
+                    '$sum': 1
+                }
+            }
+    }, {
+            "$project": {
+                "ip": '$_id',
+                'date': 1,
+                'count': 1
+            }
+    }, {
+            "$sort": {
+                "count": -1
+            }
+    }, {
+            '$limit': 10
+    }]):
+        print(row.get('ip'))
